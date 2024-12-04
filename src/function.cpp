c94beb8ffaa3342287a701f89d6f229afa942071
AlessandroRuzza <alessandroruzza22@gmail.com>
2024-12-04 21:18:25 +0000

    Fixed ill-capture of this in lambdas

--- src/function.cpp
+++ src/function.cpp
@@ -1,38 +1,43 @@
 #include "../include/function.hpp"
 
 Function Function::operator +(const Function& f) const{
-    fun resultFunction = [this, f](double x) -> double {
-        return this->value(x) + f.value(x);
+    const Function& thisFun = *this;
+
+    fun resultFunction = [thisFun, f](double x) -> double {
+        return thisFun.value(x) + f.value(x);
     };
 
-    fun resultGradient = [this, f](double x) -> double {
-        return this->grad(x) + f.grad(x);
+    fun resultGradient = [thisFun, f](double x) -> double {
+        return thisFun.grad(x) + f.grad(x);
     };
     
     return Function(resultFunction, resultGradient);
 }
 
 Function Function::operator *(const Function& f) const{
-    fun resultFunction = [this, f](double x) -> double {
-        return this->value(x) * f.value(x);
+    const Function& thisFun = *this;
+
+    fun resultFunction = [thisFun, f](double x) -> double {
+        return thisFun.value(x) * f.value(x);
     };
 
     //grad(a*b) = a*grad(b) + grad(a)*b
-    fun resultGradient = [this, f](double x) -> double {
-        return this->value(x)*f.grad(x) + this->grad(x)*f.value(x);
+    fun resultGradient = [thisFun, f](double x) -> double {
+        return thisFun.value(x)*f.grad(x) + thisFun.grad(x)*f.value(x);
     };
     
     return Function(resultFunction, resultGradient);
 }
 
 Function Function::operator *(const double k) const {
-    fun resultFunction = [this, k](double x) -> double {
-        return k*this->value(x);
+    const Function& thisFun = *this;
+
+    fun resultFunction = [thisFun, k](double x) -> double {
+        return k*thisFun.value(x);
     };  
 
-    //grad(a*b) = a*grad(b) + grad(a)*b
-    fun resultGradient = [this, k](double x) -> double {
-        return k*this->grad(x);
+    fun resultGradient = [thisFun, k](double x) -> double {
+        return k*thisFun.grad(x);
     };
 
     return Function(resultFunction, resultGradient);
