bbad855420b3017a85198be2f56388be7d0b9e68
AlessandroRuzza <alessandroruzza22@gmail.com>
2024-12-04 10:38:57 +0000

    Implement matrix-matrix operations

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -6,17 +6,24 @@
 class Matrix {
 
     private:
+    int N;
     std::vector<std::vector<double>> matrix;
 
     public:
-    Matrix(int N): matrix(N, std::vector<double>(N, 0.0)) {};
+    Matrix(int N): N(N), matrix(N, std::vector<double>(N, 0.0)) {};
     
     // Add contribution from only 1 segment of mesh to the actual matrix
-    void add_contributions(Matrix small_matrix);
+    void add_contribution(int pos, Matrix small_matrix);
 
     inline double& operator ()(int i, int j) {
         return matrix[i][j];
     }
+    inline int size() const { return N; }
+
+    Matrix& operator +=(Matrix& b);
+    Matrix& operator -=(Matrix& b);
+    Matrix operator +(Matrix& b) const;
+    Matrix operator -(Matrix& b) const;
 
 };
 
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,7 +1,50 @@
-#include "matrix.cpp"
+#include "matrix.hpp"
 
-void Matrix::add_contributions(Matrix small_matrix) {
+#include <iostream>
+
+void Matrix::add_contribution(int pos, Matrix small_matrix) {
 
     // TODO: forse da implementare
 
-};
+}
+
+Matrix& Matrix::operator+=(Matrix &b)
+{
+    if(N != b.size())
+        std::cerr << "b.size != this.size";
+
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] += b.matrix[i][j];
+        }
+
+    return *this;
+}
+
+Matrix& Matrix::operator-=(Matrix &b)
+{
+    if(N != b.size())
+        std::cerr << "b.size != this.size";
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            if(b(i,j) != 0)
+                (*this)(i,j) += b(i,j);
+        }
+
+    return *this;
+}
+
+Matrix Matrix::operator +(Matrix& b) const {
+    Matrix result(*this);
+    result += b;
+    return result;
+}
+Matrix Matrix::operator -(Matrix& b) const {
+    Matrix result(*this);
+    result -= b;
+    return result;
+}
