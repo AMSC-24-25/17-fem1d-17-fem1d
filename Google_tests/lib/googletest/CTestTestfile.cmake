74cd067045f6dc8e354c37598fb38089e7973ca9
AlessandroRuzza <alessandroruzza22@gmail.com>
2024-12-11 20:01:50 +0000

    Clear up newly ignored files
    Those files are now ignored in .gitignore, but were previously
    committed.
    They are Makefiles *.pc *.cmake, all files generated by cmake or make

--- Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/federico/Shared/Projects/17-fem1d-17-fem1d/Google_tests/lib/googletest
-# Build directory: /media/federico/Shared/Projects/17-fem1d-17-fem1d/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
